6985c5f5a4842d607816c48d1093794e29efe5fa
tobao17 <phamtobao99@gmail.com>
2020-08-26 23:29:00 +0700

    Updates

---
+++ precache-manifest.a6b74706648d9350764c7372692d9d46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84daa88e72fafa0f183f1b212f2df3dc",
+    "url": "/simpleLove/index.html"
+  },
+  {
+    "revision": "7b78e5425dc25b3d4b17",
+    "url": "/simpleLove/static/css/main.995fcaa0.chunk.css"
+  },
+  {
+    "revision": "8a4ca60ecc4d3c0ed137",
+    "url": "/simpleLove/static/js/2.2b869574.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/simpleLove/static/js/2.2b869574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b78e5425dc25b3d4b17",
+    "url": "/simpleLove/static/js/main.6625fa44.chunk.js"
+  },
+  {
+    "revision": "e94713510c647a85c6a9",
+    "url": "/simpleLove/static/js/runtime-main.7631771e.js"
+  },
+  {
+    "revision": "b595984eb99e8e1c285b8a7c0229b63d",
+    "url": "/simpleLove/static/media/avatar.b595984e.png"
+  },
+  {
+    "revision": "adb483a4179818b2de0ad0d409b2441d",
+    "url": "/simpleLove/static/media/background6.adb483a4.jpg"
+  },
+  {
+    "revision": "d7f5076152373e99eb7865373297b56b",
+    "url": "/simpleLove/static/media/heart3.d7f50761.png"
+  },
+  {
+    "revision": "c7ee1bb5c8b72f45c121cb567216de65",
+    "url": "/simpleLove/static/media/mygirl.c7ee1bb5.png"
+  }
+]);
